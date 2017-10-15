504d3e59bb5cf5d1461c35e0f08f9bd36430ff8f
Javril Bavogui <javrilbavogui@yahoo.ca>
2017-10-15 19:08:43 -0400

    Commit 3

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '67.205.153.116'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'nglaravel'),
             'username' => env('DB_USERNAME', 'panziatecuser'),
